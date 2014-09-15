2a60b4f674239af455ff8b2aba4d041a8c6936b8
Kristian Øllegaard <kristian@oellegaard.com>
2014-09-15 21:37:20 +0200

    Update script.py

--- script.py
+++ script.py
@@ -2,19 +2,28 @@ import urllib2
 import yaml
 import re
 import boto
+import os
+
+CREDENTIALS_DIR = "/credentials"
+
+conn = boto.connect_s3()
+
 
 user_data_string = urllib2.urlopen("http://169.254.169.254/2012-01-12/user-data/").read()
 user_data = dict([s.replace("export ", "").split("=") for s in re.findall("(?:\s)?(export \w+\=[\w_]+)+", user_data_string)])
 
-conn = boto.connect_s3()
-
-bucket = conn.get_bucket('ls-%(CLOUD_ENVIRONMENT)s-credentials' % user_data, validate=False) # Must have validate false, because of strict IAM rules
+# Must have validate false, because of strict IAM rules
+bucket = conn.get_bucket('ls-%(CLOUD_ENVIRONMENT)s-credentials' % user_data, validate=False)
 user_files = list(bucket.list("%(CLOUD_DEV_PHASE)s/%(CLOUD_APP)s/" % user_data))
+
 for f in user_files:
     local_file = f.name.split("/")[-1]
-    f.get_contents_to_filename("/credentials/%s" % local_file)
+    f.get_contents_to_filename("%s/%s" % (CREDENTIALS_DIR, local_file))
 
-with open("/credentials/settings.yml") as settings_file:
+if not os.path.exists(CREDENTIALS_DIR):
+    os.makedirs(CREDENTIALS_DIR)
+
+with open("%/settings.yml" % CREDENTIALS_DIR) as settings_file:
     user_data.update(yaml.safe_load(settings_file))
 
 with open("/tmp/envvars", "w") as envvar_file:
